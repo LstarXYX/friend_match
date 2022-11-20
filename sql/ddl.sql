6b833c2d5307dfdec337000c3fc57c46a25222d1
lstar <294554846@qq.com>
2022-11-20 12:31:08 +0800

    init

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -1,45 +1,41 @@
 -- 创建库
-create database if not exists my_db;
+create database if not exists friend_match;
 
 -- 切换库
-use my_db;
+use friend_match;
 
--- 用户表
-create table if not exists user
-(
-    id           bigint auto_increment comment 'id' primary key,
-    userName     varchar(256)                           null comment '用户昵称',
-    userAccount  varchar(256)                           not null comment '账号',
-    userAvatar   varchar(1024)                          null comment '用户头像',
-    gender       tinyint                                null comment '性别',
-    userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
-    userPassword varchar(512)                           not null comment '密码',
-    createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete     tinyint      default 0                 not null comment '是否删除',
-    constraint uni_userAccount
-        unique (userAccount)
-) comment '用户';
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`(
+                       `rec_id` BIGINT(20) AUTO_INCREMENT COMMENT 'id'
+                           `user_name` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '昵称',
+                       `user_account` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '账号',
+                       `avatar_url` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '头像地址',
+                       `gender` TINYINT NOT NULL DEFAULT 0 COMMENT '性别',
+                       `password` VARCHAR(512) NOT NULL COMMENT '密码',
+                       `phone` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '电话',
+                       `email` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '邮箱',
+                       `status` INT(11) NOT NULL DEFAULT 0 COMMENT '状态',
+                       `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                       `modify_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                       `is_delete` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否删除',
+                       `user_role` INT(11) NOT NULL DEFAULT 0 COMMENT '用户角色',
+                       `tags` VARCHAR(1024) NOT NULL DEFAULT '' COMMENT '标签'
+                           PRIMARY KEY (`rec_id`)
+                           UNIQUE KEY `UX_USER_NAME`(`user_name`)
+)ENGINE = INNODB DEFAULT CHARSET=utf8  COMMENT '用户表';
 
--- 帖子表
-create table if not exists post
-(
-    id            bigint auto_increment comment 'id' primary key,
-    age           int comment '年龄',
-    gender        tinyint  default 0                 not null comment '性别（0-男, 1-女）',
-    education     varchar(512)                       null comment '学历',
-    place         varchar(512)                       null comment '地点',
-    job           varchar(512)                       null comment '职业',
-    contact       varchar(512)                       null comment '联系方式',
-    loveExp       varchar(512)                       null comment '感情经历',
-    content       text                               null comment '内容（个人介绍）',
-    photo         varchar(1024)                      null comment '照片地址',
-    reviewStatus  int      default 0                 not null comment '状态（0-待审核, 1-通过, 2-拒绝）',
-    reviewMessage varchar(512)                       null comment '审核信息',
-    viewNum       int                                not null default 0 comment '浏览数',
-    thumbNum      int                                not null default 0 comment '点赞数',
-    userId        bigint                             not null comment '创建用户 id',
-    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    isDelete      tinyint  default 0                 not null comment '是否删除'
-) comment '帖子';
+
+DROP TABLE IF EXISTS `tag`;
+CREATE TABLE `tag`(
+                      `rec_id` BIGINT(20) AUTO_INCREMENT COMMENT 'id'
+                          `tag_name` VARCHAR(256) NOT NULL DEFAULT '' COMMENT '标签名称',
+                      `user_id` BIGINT(20) NOT NULL COMMENT '创建的用户id',
+                      `parent_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT '父标签id',
+                      `is_parent` TINYINT NOT NULL DEFAULT 0 COMMENT '是否父标签 0-否 1-是',
+                      `create_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+                      `modify_time` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+                      `is_delete` TINYINT(1) NOT NULL DEFAULT 0 COMMENT '是否删除',
+                      PRIMARY KEY (`rec_id`)
+                          UNIQUE KEY `UX_TAG_NAME`(`tag_name`),
+                      KEY `IX_USER_ID`(`user_id`)
+)ENGINE = INNODB DEFAULT CHARSET=utf8  COMMENT '标签表';
